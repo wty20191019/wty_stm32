9f2907da3941896292ce9edc82cf0e45eee33f89
wty20191019 <906938998@qq.com>
2026-03-07 15:33:48 +0800

    Motoer fix gpio Signed-off-by: wty20191019 <906938998@qq.com>

--- Hardware/Motor/Motor.c
+++ Hardware/Motor/Motor.c
@@ -5,47 +5,49 @@
 void Motor_Init(void)
 {
     
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
     
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0| GPIO_Pin_1 |GPIO_Pin_15 ;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4| GPIO_Pin_5;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
     
 }
 
-void Motor_SetSpeed_TIM2_ch1(int8_t ch1)
+void Motor_SetSpeed_TIM2_ch1_PWMA(int8_t ch1)
 {
     if (ch1 >= 0)
     {
-        GPIO_SetBits(GPIOA, GPIO_Pin_1);
-        GPIO_ResetBits(GPIOA, GPIO_Pin_2);
+        GPIO_SetBits(GPIOB, GPIO_Pin_4);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_5);
         TIM_SetCompare1(TIM2, ch1);
     }
     else
     {
-        GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-        GPIO_SetBits(GPIOA, GPIO_Pin_2);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_4);
+        GPIO_SetBits(GPIOB, GPIO_Pin_5);
         TIM_SetCompare1(TIM2, -ch1);
     }
 }
 
-void Motor_SetSpeed_TIM2_ch2(int8_t ch2)
+
+
+void Motor_SetSpeed_TIM2_ch2_PWMB(int8_t ch2)
 {
     if (ch2 >= 0)
     {
-        GPIO_SetBits(GPIOC, GPIO_Pin_15);
-        GPIO_ResetBits(GPIOC, GPIO_Pin_14);
+        GPIO_SetBits(GPIOA, GPIO_Pin_15);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_3);
         TIM_SetCompare1(TIM2, ch2);
     }
     else
     {
-        GPIO_ResetBits(GPIOC, GPIO_Pin_15);
-        GPIO_SetBits(GPIOC, GPIO_Pin_14);
+        GPIO_ResetBits(GPIOA, GPIO_Pin_15);
+        GPIO_SetBits(GPIOB, GPIO_Pin_3);
         TIM_SetCompare1(TIM2, -ch2);
     }
 }
--- Hardware/Motor/Motor.h
+++ Hardware/Motor/Motor.h
@@ -1,8 +1,24 @@
 #ifndef __MOTOR_H
 #define __MOTOR_H
 
+//===============================
+/*
+PWMA    (PA0 )      ch1
+AN1     (PB4 )
+AN2     (PB5 )
+
+PWMB    (PA1 )      ch2
+BN1     (PA15)
+BN2     (PB3 )
+
+
+
+*/
+//=================
+
+
 void Motor_Init(void);
-void Motor_SetSpeed_TIM2_ch1(int8_t ch1);
-void Motor_SetSpeed_TIM2_ch2(int8_t ch2);
+void Motor_SetSpeed_TIM2_ch1_PWMA(int8_t ch1);
+void Motor_SetSpeed_TIM2_ch2_PWMB(int8_t ch2);
 
 #endif
